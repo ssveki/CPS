94781372a604811204479412febb099cda524e1e
ssveki <sosninav4@gmail.com>
2025-10-09 17:03:08 +0900

    fixed indents, added swiper(not working)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+import Swiper from "swiper";
+import Pagination from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 
+const swiper = new Swiper(".swiper", {
+    modules: [Pagination],
+    direction: "horizontal",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+    }
+});
