4ddee159ec3ee56b0ad3fd0e16c8e4d4bd373ea2
ssveki <sosninav4@gmail.com>
2025-10-30 14:31:17 +0900

    created menu

--- src/main.js
+++ src/main.js
@@ -3,3 +3,15 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
     },
     });
+
+const burgerButton = document.querySelector('.burger');
+const closeButton = document.querySelector('.close-btn');
+const sideMenu = document.querySelector('.sideMenu');
+
+burgerButton.addEventListener('click', function() {
+    sideMenu.style.display = 'block';
+});
+
+closeButton.addEventListener('click', function() {
+    sideMenu.style.display = 'none';
+});
